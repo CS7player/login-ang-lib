d817ea4fcc32a001341d9682afe5dbf00be03e8b
chandra <s.chandrasekhar.h@gmail.com>
2025-05-24 10:44:03 +0530

    setup001

---
+++ projects/login-ang-lib/src/lib/components/text-field/text-field.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-text-field',
+  imports: [],
+  templateUrl: './text-field.component.html',
+  styleUrl: './text-field.component.css'
+})
+export class TextFieldComponent {
+
+}
---
+++ projects/login-ang-lib/src/public-api.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of login-ang-lib
+ */
+
+//components
+export * from './lib/components/loader/loader.component';
+
+//utils
+export * from './lib/utils/api-manager.service';
+export * from './lib/utils/ls-manager.service';
+
